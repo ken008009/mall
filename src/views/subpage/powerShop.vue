5c8847ebf24e8861b2410d36346a48ed97823484
Developer <dev@example.com>
2026-05-29 15:51:51 +0800

    更新直推奖励比例从10.5%提升至25%

--- src/views/subpage/powerShop.vue
+++ src/views/subpage/powerShop.vue
@@ -83,21 +83,21 @@ const list = [
     maxAmount: 4000,
     tokenPrice: 100,
     tokenReleaseRate: lang('（10个月）300天释放完毕'),
-    directReferralReward: '10.5%'
+    directReferralReward: '25%'
   },
   {
     minAmount: 4500,
     maxAmount: 10000,
     tokenPrice: 75,
     tokenReleaseRate: lang('（20个月）600天释放完毕'),
-    directReferralReward: '10.5%'
+    directReferralReward: '25%'
   },
   {
     minAmount: 12000,
     maxAmount: 100000,
     tokenPrice: 50,
     tokenReleaseRate: lang('（25个月）750天释放完毕'),
-    directReferralReward: '10.5%'
+    directReferralReward: '25%'
   }
 ]
 
